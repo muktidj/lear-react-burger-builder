72a6b47d2c60ef5321fe953f2f5955ebdb644ea1
muktidj <muktidwijatmoko@gmail.com>
2020-02-01 12:15:22 +0700

    Bab 8 1.Creating Layout Component

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Layout from "../src/components/Layouts/Layout"
+
+class App extends Component {
+  render() {
+  return (
+    <div>
+    <Layout>
+      <p>Test</p>
+    </Layout>
+    </div>
+  );
+}
+}
+
+export default App;
